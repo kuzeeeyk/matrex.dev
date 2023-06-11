20d583cd48e00813c98a95b7c3f525d110478441
matrex <matrexwutsu@gmail.com>
2023-06-11 15:33:38 +0300

    background color changed, some buttons added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
 
+const user = {
+  name: "Matrex",
+  imageurl:"https://cdn.discordapp.com/attachments/1117203721688977478/1117406477213241446/godtierpfp.jpg",
+  imagesize: 90,
+};
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <img
+       className="avatar"
+      src={user.imageurl}
+      alt={"Photo of " + user.name}
+      ></img>
         <p>
-          Hi.
+          Soon..
         </p>
-        <a
-          className="App-link"
-          href="https://www.instagram.com/matrex03/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Instagram
-        </a>
+           <a href='https://twitter.com/matrex_03' target='_blank' rel='noopener nopreferer'>
+            <button className="twitter">Twitter</button>
+           </a>
+           <a href='https://github.com/matrex03' target='_blank' rel='noopener nopreferer'>
+            <button className="github">Github</button>
+           </a>
+           <a href='https://discord.gg/mAffaJ2uhD' target='_blank' rel='noopener nopreferer'>
+            <button className="discord">Discord</button>
+           </a>
+           <a href='https://www.instagram.com/matrex03/' target='_blank' rel='noopener nopreferer'>
+           <button className="instagram">Instagram</button>
+           </a>
       </header>
-    </div>
+    
+      </div>
   );
 }
 
 export default App;
+
