d6a731c96db04a7dde5a29490a7116ade478928a
matrex <matrexwutsu@gmail.com>
2023-06-13 00:55:02 +0300

    added socials and about me

--- src/App.jsx
+++ src/App.jsx
@@ -10,45 +10,63 @@ const user = {
 function App() {
   return (
     <div className="App animation">
+      <div className="header">
+        <p>Aziz</p>
+      </div>
       <div className="maindiv">
+        <div className="aboutme">
+          <h2 className="title">About Me</h2>
+          <p className="description">
+            Hey, I'm a 17 year old with a passion for programming and a beginner
+            in the world of software development. I'm currently learning React
+            and eager to expand my knowledge and skills. Ever since my
+            childhood, I've been captivated by computers, and now I'm channeling
+            that interest into learning and exploring the vast realm of coding.
+            Despite being new to this field, I'm determined to persevere and
+            grow as a developer, embracing the challenges and opportunities that
+            lie ahead.
+          </p>
+        </div>
         <div className="avatar">
           <img alt="" src={user.imageurl} className="photo" />
         </div>
-        <div className="aboutme">
-          <h1>About Me</h1>
-          <p className="limit">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel
-            eveniet voluptatem provident cupiditate sapiente illo, totam
-            consequatur impedit, sequi modi exercitationem expedita architecto
-            odio quos labore tempora? Animi, tempora adipisci?
-          </p>
-        </div>
       </div>
-      <div className="links">
-        <a href="https://github.com/matrex03" target="_blank" rel="noreferrer">
-          <button className="github">Github</button>
-        </a>
-        <a
-          href="https://twitter.com/matrex_03"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button className="twitter">Twitter</button>
-        </a>
-        <a
-          href="https://discord.gg/mAffaJ2uhD"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button className="discord">Discord</button>
-        </a>
-        <a
-          href="https://www.instagram.com/matrex03/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button className="instagram">Instagram</button>
-        </a>
+      <div>
+        <h2 className="title">Social</h2>
+        <div className="links">
+          <a
+            className="button"
+            href="https://github.com/matrex03"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Github
+          </a>
+          <a
+            className="button"
+            href="https://twitter.com/matrex_03"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Twitter
+          </a>
+          <a
+            className="button"
+            href="https://discord.gg/mAffaJ2uhD"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Discord
+          </a>
+          <a
+            className="button"
+            href="https://www.instagram.com/matrex03/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Instagram
+          </a>
+        </div>
       </div>
     </div>
   );
